765413c280a37e61a58ce681c756ef22f59baeac
khaganii <xaqaniabdullayev99@gmail.com>
2020-03-10 17:23:55 +0400

    case1 case2

--- src/Controller/GetAllFlights.java
+++ src/Controller/GetAllFlights.java
@@ -4,9 +4,10 @@ import DAO.Dao;
 import Libraries.Flights;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,8 +49,11 @@ public class GetAllFlights implements Dao{
     ArrayList<Flights> flights = new ArrayList<>(read());
     StringBuilder sb = new StringBuilder();
     for (Flights flight : flights) {
-      sb.append(represent(flight));
-      sb.append("\n");
+      if (after24hours(flight.getTime()))
+      {
+        sb.append(represent(flight));
+        sb.append("\n");
+      }
     }
     return sb.toString();
   }
@@ -64,5 +68,14 @@ public class GetAllFlights implements Dao{
       }
     }
     return a;
+  }
+
+  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+  public boolean after24hours(String a){
+    LocalDateTime d1;
+    LocalDateTime dateTime = LocalDateTime.parse(a, formatter); //convert to data_time
+    LocalDateTime now = LocalDateTime.now(); //present time
+    d1 = now.plusDays(1); //after 24 h
+    return d1.isBefore(dateTime); //data_time is before d1
   }
 }
